fa505175d65f6340057fc0120f058edec8d7ba33
amirshnll <amirsh.nll@gmail.com>
2022-04-14 15:07:47 +0430

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,212 +1,200 @@
-HomeText([
-    {
-        "title":"Amir Shokri",
-        "name":"Amir Shokri",
-        "profilePic":"assets/img/avatar.jpg",
-        "location": "Islamic Republic of Iran",
-        "email":"amirsh.nll@gmail.com",
-        "about":"I have a Master's degree in <strong>artificial intelligence</strong> from <strong>Semnan University's</strong> Department of Computer and Electrical Engineering, and I did research under the direction of <strong><a target='_blank' href='https://scholar.google.com/citations?user=AM9LzzgAAAAJ' title='Dr. Farzin Yaghmaie'>Dr. Farzin Yaghmaee</a></strong>. <strong>Machine learning (ML)</strong> and <strong>image memorability</strong> are two of my primary areas of study. I've always been fascinated by the idea of a robot or machine performing tedious and time-consuming tasks while I focus on more enjoyable tasks, which is why I studied <strong>artificial intelligence</strong> at university to learn how to develop machines that perform routine tasks while humans perform creative tasks."
-    }
-]);
-Links([
-    {
-        "href":"https://github.com/amirshnll",
-        "icon":"fab fa-github",
+HomeText([{
+    "title": "Amir Shokri",
+    "name": "Amir Shokri",
+    "profilePic": "assets/img/avatar.jpg",
+    "location": "Islamic Republic of Iran",
+    "email": "amirsh.nll@gmail.com",
+    "about": "I have a Master's degree in <strong>artificial intelligence</strong> from <strong>Semnan University's</strong> Department of Computer and Electrical Engineering, and I did research under the direction of <strong><a target='_blank' href='https://scholar.google.com/citations?user=AM9LzzgAAAAJ' title='Dr. Farzin Yaghmaie'>Dr. Farzin Yaghmaee</a></strong>. <strong>Machine learning (ML)</strong> and <strong>image memorability</strong> are two of my primary areas of study. I've always been fascinated by the idea of a robot or machine performing tedious and time-consuming tasks while I focus on more enjoyable tasks, which is why I studied <strong>artificial intelligence</strong> at university to learn how to develop machines that perform routine tasks while humans perform creative tasks."
+}]);
+Links([{
+        "href": "https://github.com/amirshnll",
+        "icon": "fab fa-github",
     },
     {
-        "href":"https://www.slideshare.net/AmirShokri2/",
-        "icon":"fab fa-slideshare",
+        "href": "https://www.slideshare.net/AmirShokri2/",
+        "icon": "fab fa-slideshare",
     },
     {
-        "href":"https://www.linkedin.com/in/amirshokri/",
-        "icon":"fab fa-linkedin",
+        "href": "https://www.linkedin.com/in/amirshokri/",
+        "icon": "fab fa-linkedin",
     },
     {
-        "href":"https://www.youtube.com/channel/UCwoOSpsYCRpvqI0SM0PmzdA",
-        "icon":"fab fa-youtube",
+        "href": "https://www.youtube.com/channel/UCwoOSpsYCRpvqI0SM0PmzdA",
+        "icon": "fab fa-youtube",
     },
     {
-        "href":"https://www.kaggle.com/amirshnll",
-        "icon":"fab fa-kaggle",
+        "href": "https://www.kaggle.com/amirshnll",
+        "icon": "fab fa-kaggle",
     },
     {
-        "href":"https://scholar.google.com/citations?user=VRbi9YUAAAAJ",
-        "icon":"fal fa-graduation-cap",
+        "href": "https://scholar.google.com/citations?user=VRbi9YUAAAAJ",
+        "icon": "fal fa-graduation-cap",
     },
     {
-        "href":"https://orcid.org/0000-0003-4014-7982",
-        "icon":"fab fa-orcid",
+        "href": "https://orcid.org/0000-0003-4014-7982",
+        "icon": "fab fa-orcid",
     },
 ]);
-Skill([
-    {
-        "title":"PHP  (Advanced)",
+Skill([{
+        "title": "PHP  (Advanced)",
     },
     {
-        "title":"Java (Intermediate)",
+        "title": "Java (Intermediate)",
     },
     {
-        "title":"Matlab (Intermediate)",
+        "title": "Matlab (Intermediate)",
     },
     {
-        "title":"Python (Advanced)",
+        "title": "Python (Advanced)",
     },
     {
-        "title":"Tensorflow (Intermediate)",
+        "title": "Tensorflow (Intermediate)",
     },
     {
-        "title":"Linux (Intermediate)",
+        "title": "Linux (Intermediate)",
     },
     {
-        "title":"C & C++ (Intermediate)",
+        "title": "C & C++ (Intermediate)",
     },
     {
-        "title":"Git (Advanced)",
+        "title": "Git (Advanced)",
     },
     {
-        "title":"Machine Learning (<a href='https://coursera.org/verify/X274Z4RLNCYV' title='Coursera Certificate' target='_blank'>Coursera Certificate</a>)",
+        "title": "Machine Learning (<a href='https://coursera.org/verify/X274Z4RLNCYV' title='Coursera Certificate' target='_blank'>Coursera Certificate</a>)",
     },
 ]);
-Language([
-    {
-        "title":"English",
+Language([{
+        "title": "English",
 
     },
     {
-        "title":"Persian (Native)",
+        "title": "Persian (Native)",
 
     },
 ]);
-interest([
-    {
-        "title":"Text mining"
+interest([{
+        "title": "Text mining"
     },
     {
-        "title":"Image Processing"
+        "title": "Image Processing"
     },
     {
-        "title":"Image Memorability"
+        "title": "Image Memorability"
     },
     {
-        "title":"Machine Learning (ML)"
+        "title": "Machine Learning (ML)"
     },
     {
-        "title":"Natural Language Processing (NLP)"
+        "title": "Natural Language Processing (NLP)"
     }
 ]);
-Experience([
-    {
-        "title":"Teaching Assistant (part-time)",
-        "at":"<a href='https://semnan.ac.ir/' title='Semnan University'>Semnan University</a>, Department of Electrical and Computer",
+Experience([{
+        "title": "Teaching Assistant (part-time)",
+        "at": "<a href='https://semnan.ac.ir/' title='Semnan University'>Semnan University</a>, Department of Electrical and Computer",
         "des": "Image processing Course, Machine learning Course, Computer Architecture Course <br /> Developed a simple learning management system (LMS) on Github, Used Mailing List for Reminding class, migrated documentation from word to latex in class",
-        "startDate":"November 2020",
-        "endDate":"May 2022 (1 yr 7 mo)",
+        "startDate": "November 2020",
+        "endDate": "May 2022 (1 yr 7 mo)",
     },
     {
-        "title":"PHP Developer (full-time)",
-        "at":"<a href='https://chenco.ir/en' title='Cheshmenor'>Cheshmenor</a>, Software Department",
+        "title": "PHP Developer (full-time)",
+        "at": "<a href='https://chenco.ir/en' title='Cheshmenor'>Cheshmenor</a>, Software Department",
         "des": "Developed Leasing System and retail sales data analyze Dashboard",
-        "startDate":"January 2019",
-        "endDate":"August 2019 (8 mo)",
+        "startDate": "January 2019",
+        "endDate": "August 2019 (8 mo)",
     },
     {
-        "title":"IT Technician (internship)",
-        "at":"<a href='https://damavandwaters.com/?lang=en' title='Damavand Mineral Co'>Damavand Mineral Co</a>, R&D Department",
+        "title": "IT Technician (internship)",
+        "at": "<a href='https://damavandwaters.com/?lang=en' title='Damavand Mineral Co'>Damavand Mineral Co</a>, R&D Department",
         "des": "Developed Document Management System",
-        "startDate":"September 2018",
-        "endDate":"December 2018 (4 mo)",
+        "startDate": "September 2018",
+        "endDate": "December 2018 (4 mo)",
     },
     {
-        "title":"Web Developer (internship)",
-        "at":"<a href='https://www.parsonline.com/' title='ParsOnline Group'>ParsOnline Group</a>, Software Department",
+        "title": "Web Developer (internship)",
+        "at": "<a href='https://www.parsonline.com/' title='ParsOnline Group'>ParsOnline Group</a>, Software Department",
         "des": "Developed Task Manager System",
-        "startDate":"June 2015",
-        "endDate":"August 2015  (3 mo)",
+        "startDate": "June 2015",
+        "endDate": "August 2015  (3 mo)",
     },
     {
-        "title":"PHP Developer (full-time)",
-        "at":"<a href='http://www.spiraldesign.org/' title='Spiral Design Center'>Spiral Design Center</a>, Software Department",
+        "title": "PHP Developer (full-time)",
+        "at": "<a href='http://www.spiraldesign.org/' title='Spiral Design Center'>Spiral Design Center</a>, Software Department",
         "des": "Developed a booking WordPress plugin",
-        "startDate":"September 2014",
-        "endDate":"January 2015  (5 mo)",
+        "startDate": "September 2014",
+        "endDate": "January 2015  (5 mo)",
     }
 ]);
-Education([
-    {
-        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Semnan_University' title='Semnan University'>Semnan University</a>",
-        "study":"Artificial Intelligence and Robotics",
-        "grade":"Master's degree | <strong>GPA :</strong> 3.6 | <strong>average :</strong> 17.69 of 20.00",
-        "endDate":"2019 - 2022",
+Education([{
+        "title": "<a taget='_blank' href='https://en.wikipedia.org/wiki/Semnan_University' title='Semnan University'>Semnan University</a>",
+        "study": "Artificial Intelligence and Robotics",
+        "grade": "Master's degree | <strong>GPA :</strong> 3.6 | <strong>average :</strong> 17.69 of 20.00",
+        "endDate": "2019 - 2022",
     },
     {
-        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Shamsipour_Technical_College' title='Shamsipour Technical and Vocational University'>Shamsipour Technical and Vocational University</a>",
-        "study":"Computer Software Technology",
-        "grade":"Bachelor's degree | <strong>GPA :</strong> 3.6 | <strong>average :</strong> 18.13 of 20.00",
-        "endDate":"2017 - 2019",
+        "title": "<a taget='_blank' href='https://en.wikipedia.org/wiki/Shamsipour_Technical_College' title='Shamsipour Technical and Vocational University'>Shamsipour Technical and Vocational University</a>",
+        "study": "Computer Software Technology",
+        "grade": "Bachelor's degree | <strong>GPA :</strong> 3.6 | <strong>average :</strong> 18.13 of 20.00",
+        "endDate": "2017 - 2019",
     },
     {
-        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Enghel%C4%81b-e_Esl%C4%81mi_Technical_College' title='Enghelāb-e Eslāmi Technical College'>Enghelāb-e Eslāmi Technical College</a>",
-        "study":"Computer Software Engineering",
-        "grade":"Associate's degree | <strong>GPA :</strong> 3.3 | <strong>average :</strong> 17.06 of 20.00",
-        "endDate":"2014 - 2017",
+        "title": "<a taget='_blank' href='https://en.wikipedia.org/wiki/Enghel%C4%81b-e_Esl%C4%81mi_Technical_College' title='Enghelāb-e Eslāmi Technical College'>Enghelāb-e Eslāmi Technical College</a>",
+        "study": "Computer Software Engineering",
+        "grade": "Associate's degree | <strong>GPA :</strong> 3.3 | <strong>average :</strong> 17.06 of 20.00",
+        "endDate": "2014 - 2017",
     },
     {
-        "title":"Dr-Hessabi Highschool",
-        "study":"Master of Computer Applications (MCA)",
-        "grade":"Diploma | <strong>average :</strong> 17.63 of 20.00",
-        "endDate":"2011 - 2014",
+        "title": "Dr-Hessabi Highschool",
+        "study": "Master of Computer Applications (MCA)",
+        "grade": "Diploma | <strong>average :</strong> 17.63 of 20.00",
+        "endDate": "2011 - 2014",
     }
 ]);
-Project([
-    {
-        "title":"camelCase.ir",
-        "link":"https://camelcase.ir",
-        "href":"https://camelcase.ir",
-        "des":"<span class='fal fa-diamond'></span> Educational Blog (Programming & AI), <span class='fal fa-crown'></span> Alexa Rank : Top 3000 top website in Iran, <span class='fal fa-globe'></span> 1100 Free Blog Post"
+Project([{
+        "title": "camelCase.ir",
+        "link": "https://camelcase.ir",
+        "href": "https://camelcase.ir",
+        "des": "<span class='fal fa-diamond'></span> Educational Blog (Programming & AI), <span class='fal fa-crown'></span> Alexa Rank : Top 3000 top website in Iran, <span class='fal fa-globe'></span> 1100 Free Blog Post"
     },
     {
-        "title":"Persian Swear Words Dataset <span class='fal fa-star'></span> 170 star",
-        "link":"https://github.com/amirshnll/Persian-Swear-Words",
-        "href":"https://github.com/amirshnll/Persian-Swear-Words",
-        "des":"List of Persian Swears, DOI: <strong>10.34740/kaggle/dsv/2094967</strong>"
+        "title": "Persian Swear Words Dataset <span class='fal fa-star'></span> 175 star",
+        "link": "https://github.com/amirshnll/Persian-Swear-Words",
+        "href": "https://github.com/amirshnll/Persian-Swear-Words",
+        "des": "List of Persian Swears, DOI: <strong>10.34740/kaggle/dsv/2094967</strong>"
     },
     {
-        "title":"right to be forgotten (RTBF) Directory <span class='fal fa-star'></span> 120 star",
-        "link":"https://rtbf.ir",
-        "href":"https://rtbf.ir",
-        "des":"A directory of direct links to delete your account from iranian web services. | github repository : <strong><a href='https://github.com/amirshnll/rtbf.ir' title='amirshnll/rtbf.ir'>amirshnll/rtbf.ir</a></strong>"
+        "title": "right to be forgotten (RTBF) <span class='fal fa-star'></span> 133 star",
+        "link": "https://rtbf.ir",
+        "href": "https://rtbf.ir",
+        "des": "A directory of direct links to delete your account from iranian web services. | github repository : <strong><a href='https://github.com/rtbf.ir/rtbf.ir' title='amirshnll/rtbf.ir'>amirshnll/rtbf.ir</a></strong>"
     },
     {
-        "title":"Datalk Podcast (Data + [Ta] + lk) - Persian Podcast",
-        "link":"https://anchor.fm/datatalk",
-        "href":"https://anchor.fm/datatalk",
-        "des":"Subjects: Machine Learning, Data Science, Data privacy, Data Security"
-    },
-]);
-Reeferences([
-    {
-        "title":"Farzin Yaghmaee",
-        "pos":"Assistant Professor of Electrical and Computer Engineering Department, Semnan University, Iran",
-        "mail":"f_yaghmaee@semnan.ac.ir",
-        "gsc":"https://scholar.google.com/citations?user=bE1vyBwAAAAJ&hl=en"
-    },
-    {
-        "title":"Mohammad Javad Fadaeieslam ",
-        "pos":"Assistant Professor of Electrical and Computer Engineering Department, Semnan University, Iran",
-        "mail":"fadaei@semnan.ac.ir",
-        "gsc":"https://scholar.google.com/citations?user=VO3UT_UAAAAJ"
+        "title": "Datalk Podcast (Data + [Ta] + lk) - Persian Podcast",
+        "link": "https://anchor.fm/datatalk",
+        "href": "https://anchor.fm/datatalk",
+        "des": "Subjects: Machine Learning, Data Science, Data privacy, Data Security"
     },
 ]);
-Certificate([
+Reeferences([{
+        "title": "Farzin Yaghmaee",
+        "pos": "Assistant Professor of Electrical and Computer Engineering Department, Semnan University, Iran",
+        "mail": "f_yaghmaee@semnan.ac.ir",
+        "gsc": "https://scholar.google.com/citations?user=bE1vyBwAAAAJ&hl=en"
+    },
     {
-        "name":"Machine Learning",
-        "org":"Coursera",
-        "date":"March 2022",
-        "exp":"This certification does not expire",
-        "id":"X274Z4RLNCYV",
-        "url":"https://www.coursera.org/account/accomplishments/certificate/X274Z4RLNCYV"
+        "title": "Mohammad Javad Fadaeieslam ",
+        "pos": "Assistant Professor of Electrical and Computer Engineering Department, Semnan University, Iran",
+        "mail": "fadaei@semnan.ac.ir",
+        "gsc": "https://scholar.google.com/citations?user=VO3UT_UAAAAJ"
     },
 ]);
+Certificate([{
+    "name": "Machine Learning",
+    "org": "Coursera",
+    "date": "March 2022",
+    "exp": "This certification does not expire",
+    "id": "X274Z4RLNCYV",
+    "url": "https://www.coursera.org/account/accomplishments/certificate/X274Z4RLNCYV"
+}, ]);
 Article([
     /*{
         "title":"",
